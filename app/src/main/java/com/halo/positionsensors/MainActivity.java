e7d319b4fe3768ef003a3d5368732fdc3d963215
Diego Ramírez <diegormz97@outlook.com>
2018-11-13 08:54:22 -0600

    Commit: ALL CODE ADDED

---
+++ app/src/main/java/com/halo/positionsensors/MainActivity.java
@@ -0,0 +1,138 @@
+package com.halo.positionsensors;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.renderscript.RenderScript;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private ImageView mPointer;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Sensor mMagnetometer;
+    private float[] mLastAccelerometer = new float[3];
+    private float[] mLastMagnetometer = new float[3];
+    private boolean mLastAccelerometerSet = false;
+    private boolean mLastMagnetometerSet = false;
+    private float[] mR = new float[9];
+    private float[] mOrientation = new float[3];
+    private float mCurrentDegree = 0f;
+
+    private TextView tvAzimuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        mPointer = (ImageView) findViewById(R.id.pointer);
+        tvAzimuth = (TextView) findViewById(R.id.tvAzimuth);
+
+        flashLightCameraOn();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+        mSensorManager.unregisterListener(this, mMagnetometer);
+
+        //Turns off the flashlight when there is no battery
+        flashLightCameraOff();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == mAccelerometer) {
+            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
+            mLastAccelerometerSet = true;
+        } else if (event.sensor == mMagnetometer) {
+            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
+            mLastMagnetometerSet = true;
+        }
+        if (mLastAccelerometerSet && mLastMagnetometerSet) {
+            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            SensorManager.getOrientation(mR, mOrientation);
+            float azimuthInRadians = mOrientation[0];
+            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
+
+            tvAzimuth.setText("Azimuth: "+azimuthInDegress);
+
+            String azimuthDeg = ""+azimuthInDegress;
+
+            //Only turn on flashlight when heading north
+            if(azimuthDeg.charAt(0) == '3'){
+                flashLightCameraOn();
+            }else{
+                flashLightCameraOff();
+            }
+
+            RotateAnimation ra = new RotateAnimation(
+                    mCurrentDegree,
+                    -azimuthInDegress,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF,
+                    0.5f);
+
+            ra.setDuration(250);
+
+            ra.setFillAfter(true);
+
+            mPointer.startAnimation(ra);
+            mCurrentDegree = -azimuthInDegress;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void flashLightCameraOn(){
+        CameraManager camera = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+
+
+        try{
+            String cid = camera.getCameraIdList()[0];
+            camera.setTorchMode(cid,true);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void flashLightCameraOff(){
+        CameraManager camera = (CameraManager)getSystemService(Context.CAMERA_SERVICE);
+
+
+        try{
+            String cid = camera.getCameraIdList()[0];
+            camera.setTorchMode(cid,false);
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
